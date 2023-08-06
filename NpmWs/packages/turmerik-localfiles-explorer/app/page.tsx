ef9c78059822e27f4aa3ce088c964586a8357985
Daniel Tincu <tincuvd@gmail.com>
2023-08-06 15:47:49 +0300

    Added windows authentication to NextJs apps

--- NpmWs/packages/turmerik-localfiles-explorer/app/page.tsx
+++ NpmWs/packages/turmerik-localfiles-explorer/app/page.tsx
@@ -1,10 +1,5 @@
 "use client"; // This is a client component 👈🏽
 
-import { detect_mobile } from "@turmerik/next-react";
-const useIsMobile = detect_mobile.useIsMobile;
-
 export default function Home() {
-  const isMobile = useIsMobile();
-
-  return isMobile ? <div>Mobile variant</div> : <div>Desktop variant</div>;
+  return <div>Home page</div>;
 }
--- NpmWs/packages/turmerik-localfiles-explorer/pages/api/AppSettings.ts
+++ NpmWs/packages/turmerik-localfiles-explorer/pages/api/AppSettings.ts
@@ -1,3 +1,16 @@
+import { sso } from "node-expose-sspi"
+
+const ssoAuth = sso.auth()
+
 export default function handler(req: any, res: any) {
-  res.status(200).json({ name: "John Doe" })
+  return new Promise<void>((resolve, reject) => {
+    try {
+      return ssoAuth(req, res, () => {
+        res.json({ sso: req.sso })
+        resolve()
+      })
+    } catch (error) {
+      reject(error)
+    }
+  })
 }
